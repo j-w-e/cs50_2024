b3011eccf3bbd40fe70182f796d50f3fdd606b1d
Hugh Earp <hugh.earp@gmail.com>
2024-01-13 11:53:12 +0100

    testing the score function

--- wk2/scrabble/scrabble.c
+++ wk2/scrabble/scrabble.c
@@ -2,7 +2,24 @@
 #include <stdio.h>
 #include <string.h>
 
+const int LETTER_VALUES[] = { 1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10 }
+
 int main(void) 
 {
-  word1 = get_string();
+    string word1 = get_string("Player 1: ");
+    /* string word2 = get_string("Player 2: "); */
+    printf("That scores %i\n", score(word1));
+    return 0;
+}
+
+int score(string word)
+{
+    int word_length = strlen(word);
+    int score = 0;
+
+    for (int i = 0; i < word_length; i++)
+    {
+        score += LETTER_VALUES[word[i] - 65];
+    }
+    return (score);
 }
