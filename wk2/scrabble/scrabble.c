e2483132e636fb47ec4d20dbd4be58354075888b
Hugh Earp <hugh.earp@gmail.com>
2024-01-13 11:55:24 +0100

    forgot prototype

--- wk2/scrabble/scrabble.c
+++ wk2/scrabble/scrabble.c
@@ -4,7 +4,9 @@
 
 const int LETTER_VALUES[] = { 1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10 };
 
-int main(void) 
+int score(string word);
+
+int main(void)
 {
     string word1 = get_string("Player 1: ");
     /* string word2 = get_string("Player 2: "); */
