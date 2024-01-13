09ca4057d2152d18050754a8907a687920a16bd2
j-w-e <109866648+j-w-e@users.noreply.github.com>
2024-01-13 18:52:57 +0000

    Sat, Jan 13, 2024, 7:52 PM +01:00

--- wk2/readability/readability.c
+++ wk2/readability/readability.c
@@ -9,8 +9,8 @@ float calculate_read_level(float sentences, float word_length);
 
 int main(void)
 {
-    string text = get_string("Text: ");
-    // string text = "Alice was beginning to get very tired of sitting by her sister on the bank, and of having nothing to do: once or twice she had peeped into the book her sister was reading, but it had no pictures or conversations in it, \"and what is the use of a book,\" thought Alice \"without pictures or conversation?\"";
+    /* string text = get_string("Text: "); */
+    string text = "Alice was beginning to get very tired of sitting by her sister on the bank, and of having nothing to do: once or twice she had peeped into the book her sister was reading, but it had no pictures or conversations in it, \"and what is the use of a book,\" thought Alice \"without pictures or conversation?\"";
     int text_length = strlen(text);
 
     // function to calculate the number of sentences per 100 words
@@ -42,15 +42,10 @@ float count_sentences(string text, int text_length)
         if (text[i] == '.' || text[i] == '!' || text[i] == '?')
         {
             if (i > 0 & text[i - 1] != '.')
-                potential_new_sentence = 1;
+                sentences++;
         } else if (isspace(text[i]))
         {
             words++;
-        }
-        else if (potential_new_sentence & isalpha(text[i]))
-        {
-            potential_new_sentence = 0;
-            sentences++;
         }
     }
 
