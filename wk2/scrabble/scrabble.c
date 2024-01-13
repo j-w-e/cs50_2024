ca1f94362d160a2d201fe25bc263b542cd92bd2f
j-w-e <109866648+j-w-e@users.noreply.github.com>
2024-01-13 13:12:00 +0000

    Sat, Jan 13, 2024, 2:12 PM +01:00

--- wk2/scrabble/scrabble.c
+++ wk2/scrabble/scrabble.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <string.h>
 
-const int LETTER_VALUES[] = { 1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10 };
+const int LETTER_VALUES[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 
 int score(string word);
 
