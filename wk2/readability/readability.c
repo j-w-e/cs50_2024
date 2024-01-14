a8467132a2e0b8b8d892c101076d19da1a99cc21
j-w-e <109866648+j-w-e@users.noreply.github.com>
2024-01-14 07:05:38 +0000

    Sun, Jan 14, 2024, 8:05 AM +01:00

--- wk2/readability/readability.c
+++ wk2/readability/readability.c
@@ -8,7 +8,9 @@ float calculate_read_level(float sentences, float word_length);
 int main(void)
 {
     string text = get_string("Text: ");
-    // string text = "Alice was beginning to get very tired of sitting by her sister on the bank, and of having nothing to do: once or twice she had peeped into the book her sister was reading, but it had no pictures or conversations in it, \"and what is the use of a book,\" thought Alice \"without pictures or conversation?\"";
+    // string text = "Alice was beginning to get very tired of sitting by her sister on the bank, and of having nothing to do: once
+    // or twice she had peeped into the book her sister was reading, but it had no pictures or conversations in it, \"and what is
+    // the use of a book,\" thought Alice \"without pictures or conversation?\"";
 
     int text_length = strlen(text);
     int words = 1;
@@ -19,12 +21,12 @@ int main(void)
     {
         if (isalpha(text[i]))
             letters++;
-         else if (isspace(text[i]))
-                words++;
-         else if (text[i] == '.' || text[i] == '!' || text[i] == '?')
-                  sentences++;
+        else if (isspace(text[i]))
+            words++;
+        else if (text[i] == '.' || text[i] == '!' || text[i] == '?')
+            sentences++;
     }
-    sentences =  sentences * 100.0 / words;
+    sentences = sentences * 100.0 / words;
     letters = (float) letters / words * 100;
 
     float score = calculate_read_level(sentences, letters);
