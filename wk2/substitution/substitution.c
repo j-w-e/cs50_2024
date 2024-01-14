750cbaec9d45fcc958ac485592e25e691a8a4bb1
j-w-e <109866648+j-w-e@users.noreply.github.com>
2024-01-14 08:10:48 +0000

    Sun, Jan 14, 2024, 9:10 AM +01:00

--- wk2/substitution/substitution.c
+++ wk2/substitution/substitution.c
@@ -37,16 +37,17 @@ int is_key_invalid(string key)
         return 1; // if key is not 26 characters long
 
     string is_each_letter_used = "00000000000000000000000000"; // 26 zeros
-printf("%s is key\n", key);
+
     for (int i = 0; i < keylength; i++)
     {
         if (!isalpha(key[i]))
             return 2; // key must only contain letters
         char c = toupper(key[i]);
+        int d = is_each_letter_used[c - 64];
+        int e = is_each_letter_used[c - 65];
         if (is_each_letter_used[c - 64])
             return 3;
         is_each_letter_used[c - 64] = 1;
-        printf("%s\n", is_each_letter_used);
     }
 
     return 0; // key is valid
