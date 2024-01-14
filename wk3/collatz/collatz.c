cf74dd9f69e212ecbc118555fb8191b1682381a3
Hugh Earp <hugh.earp@gmail.com>
2024-01-14 20:49:04 +0100

    adding collatz draft

--- wk3/collatz/collatz.c
+++ wk3/collatz/collatz.c
@@ -1,5 +1,6 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int collatz(int number);
 
@@ -11,7 +12,7 @@ int main(int argc, string argv[])
         return 1;
     }
 
-    int collatz_value = collatz(argv[1], 0);
+    int collatz_value = collatz(atoi(argv[1]), 0);
     printf("The collatz value of %i is %i", argv[1], collatz_value);
 
     return 0;
@@ -27,5 +28,5 @@ int collatz(int number, int steps)
     else 
         collatz(n / 2, steps + 1);
 
-    return number;
+    return steps;
 }
