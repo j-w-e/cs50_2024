532fd92fb1dc37207102f7ed7dacfe07a6c56688
Hugh Earp <hugh.earp@gmail.com>
2024-02-21 17:17:20 +0100

    adding temps and max

---
+++ wk3/temps/temps.c
@@ -0,0 +1,105 @@
+// Practice working with structs
+// Practice applying sorting algorithms
+
+#include <cs50.h>
+#include <stdio.h>
+
+#define NUM_CITIES 10
+
+typedef struct
+{
+    string city;
+    int temp;
+}
+avg_temp;
+
+avg_temp temps[NUM_CITIES];
+
+void sort_cities(void);
+
+int main(void)
+{
+    temps[0].city = "Austin";
+    temps[0].temp = 97;
+
+    temps[1].city = "Boston";
+    temps[1].temp = 82;
+
+    temps[2].city = "Chicago";
+    temps[2].temp = 85;
+
+    temps[3].city = "Denver";
+    temps[3].temp = 90;
+
+    temps[4].city = "Las Vegas";
+    temps[4].temp = 105;
+
+    temps[5].city = "Los Angeles";
+    temps[5].temp = 82;
+
+    temps[6].city = "Miami";
+    temps[6].temp = 97;
+
+    temps[7].city = "New York";
+    temps[7].temp = 85;
+
+    temps[8].city = "Phoenix";
+    temps[8].temp = 107;
+
+    temps[9].city = "San Francisco";
+    temps[9].temp = 66;
+
+
+    sort_cities();
+    printf("\nAverage July Temperatures by City\n\n");
+
+    for (int i = 0; i < NUM_CITIES; i++)
+    {
+        printf("%s: %i\n", temps[i].city, temps[i].temp);
+    }
+}
+
+/* // Sort cities by temperature in descending order */
+/* // BUBBLE SORT */
+/* void sort_cities(void) */
+/* { */
+/*     avg_temp tmp; */
+/*     tmp.temp = 0; */
+/*     tmp.city = ""; */
+/*     for (int i = 0; i < NUM_CITIES - 1; i++) */
+/*     { */
+/*         for (int j = i + 1; j < NUM_CITIES; j++) */
+/*         { */
+/*             if (temps[i].temp < temps[j].temp) */
+/*             { */
+/*                 tmp = temps[i]; */
+/*                 temps[i] = temps[j]; */
+/*                 temps[j] = tmp; */
+/*             } */
+/*         } */
+/*     } */
+/* } */
+
+// Sort cities by temperature in descending order
+// INSERTION SORT
+void sort_cities(void)
+{
+    avg_temp tmp;
+    tmp.temp = 0;
+    tmp.city = "";
+    for (int i = 1; i < NUM_CITIES; i++)
+    {
+        int j = 0;
+        while (temps[j].temp > temps[i].temp && j <= i) {
+            j++;
+        }
+        if (j < i)
+        {
+            tmp = temps[i];
+            for (int k = i; k > j; k--) {
+                temps[k] = temps[k - 1];
+            }
+            temps[j] = tmp;
+        }
+    }
+}
